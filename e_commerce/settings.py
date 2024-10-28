47036ec2979f03cc4982a2bf201d52220296b08f
Saman-naruee <samannaruee@gmail.com>
2024-10-28 22:03:31 +0330

    update settigns: add app name, migrations.

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -38,8 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party
     'rest_framework',
+
+    # apps
     'store',
+    'category',
+    'user',
 ]
 
 MIDDLEWARE = [
--- user/models.py
+++ user/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.core.validators import RegexValidator
 
 class User(AbstractUser):
@@ -8,3 +8,20 @@ class User(AbstractUser):
         regex=r'^(\+98|0)\d{10}$',
         message='Phone number must start with +98 and be 13 digits long, or start with 0 and be 11 digits long'
     )], max_length=13)
+
+    groups = models.ManyToManyField(  
+        Group,  
+        related_name='custom_user_set',  # Change if needed  
+        blank=True,  
+        help_text=('The groups this user belongs to. A user will get all permissions '  
+                    'granted to each of their groups.'),  
+        verbose_name='groups',  
+    )  
+
+    user_permissions = models.ManyToManyField(  
+        Permission,  
+        related_name='custom_user_permissions',  # Change if needed  
+        blank=True,  
+        help_text='Specific permissions for this user.',  
+        verbose_name='user permissions',  
+    ) 
